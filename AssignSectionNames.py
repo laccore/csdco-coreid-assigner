dec8c41a0faf094ddf5ddfab1ed8b1bd8741957b
Alexander Stone <enderu@gmail.com>
2018-01-02 16:11:22 -0600

    Refactoring

--- AssignSectionNames.py
+++ AssignSectionNames.py
@@ -18,57 +18,16 @@ import csv
 import timeit
 import os.path
 
+version = '0.1.0'
+
 start = timeit.default_timer()
 
-debug = 1
-
-canRun = True
+debug = 0
 
 # Build the various file names
 rawFileName = sys.argv[1]
 matchedFileName = rawFileName[:len(rawFileName)-12] + '.csv' if ('unnamed' in rawFileName) else rawFileName[:len(rawFileName)-4] + '_sectionNames.csv'
 unmatchedFileName = rawFileName[:len(rawFileName)-4] + '_UNMATCHED.csv'
-
-msclData = []
-sectionList = []
-
-with open(sys.argv[1], 'r') as rawDataFile:
-	fileReader = csv.reader(rawDataFile)
-	for row in fileReader:
-		if '\ufeff' in row[0]:
-			row[0] = row[0].replace('\ufeff','')
-			if debug:
-				print('BOM found and removed.')
-		msclData.append(row)
-
-# Ideally with a GUI these would be fields that could be entered
-startRow = 2
-
-# handle failure better
-if 'SECT NUM' in msclData[0]:
-	if debug:
-		print('Section column found in column {0}.'.format(msclData[0].index('SECT NUM')+1))
-	sectionColumn = msclData[0].index('SECT NUM')
-elif 'Section' in msclData[0]:
-	if debug:
-		print('Section column found in column {0}.'.format(msclData[0].index('Section')+1))
-	sectionColumn = msclData[0].index('Section')
-else:
-	canRun = False
-	print('ERROR: Cannot find section column. Please change section number column name to \'SECT NUM\' or \'Section\'.\n')
-
-# handle failure better
-if 'Section Depth' in msclData[0]:
-	if debug:
-		print('Section depth found in column {0}.\n'.format(msclData[0].index('Section Depth')+1))
-	sectionDepth = msclData[0].index('Section Depth')
-elif 'SECT DEPTH' in msclData[0]:
-	if debug:
-		print('Section depth found in column {0}.\n'.format(msclData[0].index('SECT DEPTH')+1))
-	sectionDepth = msclData[0].index('SECT DEPTH')
-else:
-	canRun = False
-	print('ERROR: Cannot find section depth column. Please change section number column name to \'SECT DEPTH\' or \'Section Depth\'.\n')
 
 # Build the section list
 if len(sys.argv) > 2:
@@ -76,108 +35,158 @@ if len(sys.argv) > 2:
 elif os.path.isfile('coreList.csv'):
 	coreList = 'coreList.csv'
 else:
-	canRun = False
+	coreList = False
 	print('ERROR: core list was not specified and coreList.csv does not exist. Please specify the core listing file as the last argument or rename the core list file to corelist.csv.\n')
 
-if canRun:
-	with open(coreList, 'r') as sectionListFile:
-		fileReader = csv.reader(sectionListFile)
+def applyNames(rawFileName, matchedFileName, unmatchedFileName):
+	canRun = True
+	
+	msclData = []
+	sectionList = []
+
+	with open(rawFileName, 'r') as rawDataFile:
+		fileReader = csv.reader(rawDataFile)
 		for row in fileReader:
 			if '\ufeff' in row[0]:
 				row[0] = row[0].replace('\ufeff','')
-				print('BOM found and removed.')
-			sectionList.append([int(row[0]),row[1]])
+				if debug:
+					print('BOM found and removed.')
+			msclData.append(row)
 
-	# Add the filepart_section notation field to the section log
-	nSections = 1
-	for i, row in enumerate(sectionList):
-		if i == 0:
-			row.append('1_' + str(sectionList[i][0]))
-		elif i > 0:
-			if sectionList[i][0] <= sectionList[i-1][0]:
-				nSections += 1
-			row.append(str(nSections) + '_' + str(sectionList[i][0]))
+	# Ideally with a GUI these would be fields that could be entered
+	startRow = 2
 
-	# Build a dictionary for lookup from the list
-	sectionDict = {section[2]: section[1] for section in sectionList}
+	# handle failure better
+	if 'SECT NUM' in msclData[0]:
+		if debug:
+			print('Section column found in column {0}.'.format(msclData[0].index('SECT NUM')+1))
+		sectionColumn = msclData[0].index('SECT NUM')
+	elif 'Section' in msclData[0]:
+		if debug:
+			print('Section column found in column {0}.'.format(msclData[0].index('Section')+1))
+		sectionColumn = msclData[0].index('Section')
+	else:
+		canRun = False
+		print('ERROR: Cannot find section column. Please change section number column name to \'SECT NUM\' or \'Section\'.\n')
 
-	# Add the part_section notation field to the mscl data
-	nSections = 1
-	for i, row in enumerate(msclData):
-		if i == 0:
-			row.append('Part_Section')
-		elif i < startRow:
-			row.append('')
-		elif i == startRow:
-			row.append(str(nSections) + '_' + msclData[i][sectionColumn])
-		else:
-			if int(msclData[i][sectionColumn]) < int(msclData[i-1][sectionColumn]):
-				nSections += 1
-			elif ((int(msclData[i][sectionColumn]) == int(msclData[i-1][sectionColumn])) & (float(msclData[i][sectionDepth]) < float(msclData[i-1][sectionDepth]))):
-				nSections += 1
-			row.append(str(nSections) + '_' + msclData[i][sectionColumn])
+	# handle failure better
+	if 'Section Depth' in msclData[0]:
+		if debug:
+			print('Section depth found in column {0}.\n'.format(msclData[0].index('Section Depth')+1))
+		sectionDepth = msclData[0].index('Section Depth')
+	elif 'SECT DEPTH' in msclData[0]:
+		if debug:
+			print('Section depth found in column {0}.\n'.format(msclData[0].index('SECT DEPTH')+1))
+		sectionDepth = msclData[0].index('SECT DEPTH')
+	else:
+		canRun = False
+		print('ERROR: Cannot find section depth column. Please change section number column name to \'SECT DEPTH\' or \'Section Depth\'.\n')
 
-	# These will be the lists we export from
-	matchedData = []
-	unmatchedData = []
 
-	# Copy headers
-	for row in msclData[:startRow]:
-		matchedData.append(row)
-		unmatchedData.append(row)
+	if canRun:
+		with open(coreList, 'r') as sectionListFile:
+			fileReader = csv.reader(sectionListFile)
+			for row in fileReader:
+				if '\ufeff' in row[0]:
+					row[0] = row[0].replace('\ufeff','')
+					if debug:
+						print('BOM found and removed.')
+				sectionList.append([int(row[0]),row[1]])
 
-	# Build the export lists, replacing the section# with the name and removing
-	# the extra part_section column if the row was matched
-	for row in msclData[startRow:]:
-		# Does the part_section key exist in the dict?
-		if row[-1] in sectionDict.keys():
-			row[sectionColumn] = sectionDict[row[-1]]
-			del row[-1]
+		# Add the filepart_section notation field to the section log
+		nSections = 1
+		for i, row in enumerate(sectionList):
+			if i == 0:
+				row.append('1_' + str(sectionList[i][0]))
+			elif i > 0:
+				if sectionList[i][0] <= sectionList[i-1][0]:
+					nSections += 1
+				row.append(str(nSections) + '_' + str(sectionList[i][0]))
+
+		# Build a dictionary for lookup from the list
+		sectionDict = {section[2]: section[1] for section in sectionList}
+
+		# Add the part_section notation field to the mscl data
+		nSections = 1
+		for i, row in enumerate(msclData):
+			if i == 0:
+				row.append('Part_Section')
+			elif i < startRow:
+				row.append('')
+			elif i == startRow:
+				row.append(str(nSections) + '_' + msclData[i][sectionColumn])
+			else:
+				if int(msclData[i][sectionColumn]) < int(msclData[i-1][sectionColumn]):
+					nSections += 1
+				elif ((int(msclData[i][sectionColumn]) == int(msclData[i-1][sectionColumn])) & (float(msclData[i][sectionDepth]) < float(msclData[i-1][sectionDepth]))):
+					nSections += 1
+				row.append(str(nSections) + '_' + msclData[i][sectionColumn])
+
+		# These will be the lists we export from
+		matchedData = []
+		unmatchedData = []
+
+		# Copy headers
+		for row in msclData[:startRow]:
 			matchedData.append(row)
-		else:
 			unmatchedData.append(row)
 
-	# Create a set to check unique cores names
-	namedSet = set()
+		# Build the export lists, replacing the section# with the name and removing
+		# the extra part_section column if the row was matched
+		for row in msclData[startRow:]:
+			# Does the part_section key exist in the dict?
+			if row[-1] in sectionDict.keys():
+				row[sectionColumn] = sectionDict[row[-1]]
+				del row[-1]
+				matchedData.append(row)
+			else:
+				unmatchedData.append(row)
 
-	# Export matched data
-	with open(matchedFileName, 'w') as saveFile:
-		del matchedData[0][-1] # Delete the Part_Section column header
-		filewriter = csv.writer(saveFile, delimiter=',')
-		for row in matchedData:
-			namedSet.add(row[2])
+		# Create a set to check unique cores names
+		namedSet = set()
 
-			filewriter.writerow(row)
-
-	# Cleanup
-	namedSet.remove('')
-	namedSet.remove('Section')
-
-	coreNameList = list(sectionDict.values())
-	for cn in list(set(coreNameList)):
-		cnc = coreNameList.count(cn)
-		if cnc > 1:
-			print('WARNING: Core ' + cn + ' appears in ' + coreList + ' ' + str(cnc) + ' times.')
-
-
-	# Export unmatched data
-	if len(unmatchedData) > startRow:
-		with open(unmatchedFileName, 'w') as saveFile:
+		# Export matched data
+		with open(matchedFileName, 'w') as saveFile:
+			# del matchedData[0][-1] # Delete the Part_Section column header
 			filewriter = csv.writer(saveFile, delimiter=',')
-			for row in unmatchedData:
+			for row in matchedData:
+				namedSet.add(row[sectionColumn])
+
 				filewriter.writerow(row)
 
-	stop = timeit.default_timer()
+		# Cleanup
+		namedSet.remove('')
+		namedSet.remove(msclData[0][sectionColumn])
 
-	countDiff = len(set(sectionDict.values())) - len(namedSet)
-	if (countDiff > 0):
-		err = '\nWARNING: Not all cores in ' + coreList + ' were used. '
-		err = err + 'The following ' + str(countDiff) + ' core ' + ('names were' if countDiff != 1 else 'name was') + ' not used:'
-		print(err)
-		for v in list(set(sectionDict.values())):
-			if (v not in namedSet):
-				print(v)
+		coreNameList = list(sectionDict.values())
+		for cn in list(set(coreNameList)):
+			cnc = coreNameList.count(cn)
+			if cnc > 1:
+				print('WARNING: Core ' + cn + ' appears in ' + coreList + ' ' + str(cnc) + ' times.')
 
-	print('\nCompleted in',round((stop - start),2),'seconds.')
-	print(len(matchedData)-startRow,'rows had section names assigned (' + matchedFileName + ').')
-	print('There were no unmatched rows.' if len(unmatchedData) == startRow else 'There were ' + str(len(unmatchedData)-2) + ' unmatched rows (' + unmatchedFileName + ').')
+
+		# Export unmatched data
+		if len(unmatchedData) > startRow:
+			with open(unmatchedFileName, 'w') as saveFile:
+				filewriter = csv.writer(saveFile, delimiter=',')
+				for row in unmatchedData:
+					filewriter.writerow(row)
+
+		stop = timeit.default_timer()
+
+		countDiff = len(set(sectionDict.values())) - len(namedSet)
+		if (countDiff > 0):
+			err = 'WARNING: Not all cores in ' + coreList + ' were used. '
+			err = err + 'The following ' + str(countDiff) + ' core ' + ('names were' if countDiff != 1 else 'name was') + ' not used:'
+			print(err)
+			for v in list(set(sectionDict.values())):
+				if (v not in namedSet):
+					print(v)
+
+		print('Completed in',round((stop - start),2),'seconds.')
+		print(len(matchedData)-startRow,'rows had section names assigned (' + matchedFileName + ').')
+		print('There were no unmatched rows.' if len(unmatchedData) == startRow else 'There were ' + str(len(unmatchedData)-2) + ' unmatched rows (' + unmatchedFileName + ').')
+
+if coreList:
+	applyNames(rawFileName, matchedFileName, unmatchedFileName)
+	
