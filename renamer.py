ce183b3d70d89dab9a5b7dd7fd84089b5dcab533
Alexander Stone <enderu@gmail.com>
2018-01-09 13:48:45 -0600

    Trying to use python naming conventions.

--- renamer.py
+++ renamer.py
@@ -3,10 +3,11 @@ Both pandas and numpy could do this orders of magnitude more efficiently, but as
 this will be used in the field by people not familiar with python, it's using
 only built-in libraries.
 
-Arguments: rawFileName.csv [corelist.csv]
+Arguments: input_filename.csv [corelist.csv]
 
-You can specify a core list file name, but if none is specified it tries to use
-corelist.csv. It will fail if corelist.csv doesn't exist and no file is specified.
+You can specify a core list file name, but if none is specified it looks for a file
+in the same directory named corelist.csv to use. It will fail if no file is 
+specified and corelist.csv doesn't exist.
 
 Examples:
     $ python3 renamer.py DCH_MSCL_raw.csv
@@ -21,38 +22,38 @@ import timeit
 
 version = '0.2.0'
 
-start = timeit.default_timer()
+start_time = timeit.default_timer()
 
-def applyNames(rawFileName, matchedFileName, unmatchedFileName):
-    msclData = []
+def apply_names(input_filename, matched_filename, unmatched_filename):
+    mscl_data = []
     sectionList = []
 
-    with open(rawFileName, 'r') as f:
-        msclData = [r.split(',') for r in f.read().splitlines()]
+    with open(input_filename, 'r') as f:
+        mscl_data = [r.split(',') for r in f.read().splitlines()]
 
     # Ideally with a GUI these would be fields that could be entered
-    startRow = 2
+    start_row = 2
 
     # handle failure better
-    if 'SECT NUM' in msclData[0]:
-        sectionColumn = msclData[0].index('SECT NUM')
-    elif 'Section' in msclData[0]:
-        sectionColumn = msclData[0].index('Section')
+    if 'SECT NUM' in mscl_data[0]:
+        section_column = mscl_data[0].index('SECT NUM')
+    elif 'Section' in mscl_data[0]:
+        section_column = mscl_data[0].index('Section')
     else:
         print('ERROR: Cannot find section number column. Please change section number column name to \'Section\' or \'SECT NUM\'.')
-        exit(-1)
+        exit(1)
 
     # handle failure better
-    if 'Section Depth' in msclData[0]:
-        sectionDepth = msclData[0].index('Section Depth')
-    elif 'SECT DEPTH' in msclData[0]:
-        sectionDepth = msclData[0].index('SECT DEPTH')
+    if 'Section Depth' in mscl_data[0]:
+        section_depth_column = mscl_data[0].index('Section Depth')
+    elif 'SECT DEPTH' in mscl_data[0]:
+        section_depth_column = mscl_data[0].index('SECT DEPTH')
     else:
         print('ERROR: Cannot find section depth column. Please change section depth column name to \'Section Depth\' or \'SECT DEPTH\'.')
-        exit(-1)
+        exit(1)
 
     # Build the section list
-    with open(coreList, 'r') as f:
+    with open(core_list_filename, 'r') as f:
         rows = f.read().splitlines()
         sectionList = [[int(a), b] for a, b in [r.split(',') for r in rows]]
 
@@ -71,102 +72,103 @@ def applyNames(rawFileName, matchedFileName, unmatchedFileName):
 
     # Add the part_section notation field to the mscl data
     nSections = 1
-    for i, row in enumerate(msclData):
+    for i, row in enumerate(mscl_data):
         if i == 0:
             row.append('Part_Section')
-        elif i < startRow:
+        elif i < start_row:
             row.append('')
-        elif i == startRow:
-            row.append(str(nSections) + '_' + msclData[i][sectionColumn])
+        elif i == start_row:
+            row.append(str(nSections) + '_' + mscl_data[i][section_column])
         else:
-            if int(msclData[i][sectionColumn]) < int(msclData[i-1][sectionColumn]):
+            if int(mscl_data[i][section_column]) < int(mscl_data[i-1][section_column]):
                 nSections += 1
-            elif ((int(msclData[i][sectionColumn]) == int(msclData[i-1][sectionColumn])) & (float(msclData[i][sectionDepth]) < float(msclData[i-1][sectionDepth]))):
+            elif ((int(mscl_data[i][section_column]) == int(mscl_data[i-1][section_column])) & (float(mscl_data[i][section_depth_column]) < float(mscl_data[i-1][section_depth_column]))):
                 nSections += 1
-            row.append(str(nSections) + '_' + msclData[i][sectionColumn])
+            row.append(str(nSections) + '_' + mscl_data[i][section_column])
 
     # These will be the lists we export from
-    matchedData = []
-    unmatchedData = []
+    matched_data = []
+    unmatched_data = []
 
     # Copy headers
-    for row in msclData[:startRow]:
-        matchedData.append(row[:-1])
-        unmatchedData.append(row)
+    for row in mscl_data[:start_row]:
+        matched_data.append(row[:-1])
+        unmatched_data.append(row)
 
     # Build the export lists, replacing the section# with the name and removing
     # the extra part_section column if the row was matched
-    for row in msclData[startRow:]:
+    for row in mscl_data[start_row:]:
         # Does the part_section key exist in the dict?
         if row[-1] in sectionDict.keys():
-            row[sectionColumn] = sectionDict[row[-1]]
-            matchedData.append(row[:-1])
+            row[section_column] = sectionDict[row[-1]]
+            matched_data.append(row[:-1])
         else:
-            unmatchedData.append(row)
+            unmatched_data.append(row)
     
 
     # Export matched data
-    with open(matchedFileName, 'w') as f:
-        for r in matchedData:
+    with open(matched_filename, 'w') as f:
+        for r in matched_data:
             f.write(','.join(r)+'\n')
 
     # Export unmatched data
-    if len(unmatchedData) > startRow:
-        with open(unmatchedFileName, 'w') as f:
-            for r in unmatchedData:
+    if len(unmatched_data) > start_row:
+        with open(unmatched_filename, 'w') as f:
+            for r in unmatched_data:
                 f.write(','.join(r)+'\n')
     
     # Reporting stuff
     # Create a set to check unique cores names
-    namedSet = set()
+    named_set = set()
 
-    for row in matchedData[startRow:]:
-        namedSet.add(row[sectionColumn])
+    for row in matched_data[start_row:]:
+        named_set.add(row[section_column])
 
-    coreNameList = list(sectionDict.values())
-    for cn in list(set(coreNameList)):
-        cnc = coreNameList.count(cn)
-        if cnc > 1:
-            print('WARNING: Core ' + cn + ' appears in ' + coreList + ' ' + str(cnc) + ' times.')
+    core_name_list = list(sectionDict.values())
+    for core_name in list(set(core_name_list)):
+        core_name_count = core_name_list.count(core_name)
+        if core_name_count > 1:
+            print('WARNING: Core ' + core_name + ' appears in ' + core_list_filename + ' ' + str(core_name_count) + ' times.')
 
 
-    countDiff = len(set(sectionDict.values())) - len(namedSet)
-    if (countDiff > 0):
-        err = 'WARNING: Not all cores in ' + coreList + ' were used. '
-        err = err + 'The following ' + str(countDiff) + ' core ' + ('names were' if countDiff != 1 else 'name was') + ' not used:'
-        print(err)
+    count_diff = len(set(sectionDict.values())) - len(named_set)
+    if (count_diff > 0):
+        err = 'WARNING: Not all cores in ' + core_list_filename + ' were used.\n'
+        err += 'The following ' + str(count_diff) + ' core ' + ('names were' if count_diff != 1 else 'name was') + ' not used:\n'
         for v in list(set(sectionDict.values())):
-            if (v not in namedSet):
-                print(v)
+            if (v not in named_set):
+                err += v + '\n'
+        print(err)
+        
 
-    stop = timeit.default_timer()
+    end_time = timeit.default_timer()
 
-    print(len(matchedData)-startRow,'rows had section names assigned (' + matchedFileName + ').')
-    print('There were no unmatched rows.' if len(unmatchedData) == startRow else 'There were ' + str(len(unmatchedData)-2) + ' unmatched rows (' + unmatchedFileName + ').')
-    print('Completed in',round((stop - start),2),'seconds.')    
+    print(len(matched_data)-start_row,'rows had section names assigned (' + matched_filename + ').')
+    print('There were no unmatched rows.' if len(unmatched_data) == start_row else 'There were ' + str(len(unmatched_data)-2) + ' unmatched rows (' + unmatched_filename + ').')
+    print('Completed in',round((end_time - start_time),2),'seconds.')    
 
 
 if __name__ == '__main__':
     # If zero or more than 2 parameters are passed, print correct usage and exit.
     if len(sys.argv) > 3 or len(sys.argv) == 1:
         print('Usage: python renamer.py <MSCL file.csv> (<core list.csv>)')
-        exit(-1)
+        exit(1)
     
     # Find the core list
     # If core list not specified and corelist.csv doesn't exist, tell user it's needed and what it looks like
     if len(sys.argv) > 2:
-        coreList = sys.argv[2]
+        core_list_filename = sys.argv[2]
     elif os.path.isfile('corelist.csv'):
-        coreList = 'corelist.csv'
+        core_list_filename = 'corelist.csv'
     else:
         print('A core list file needs to be specified as the last argument, or a file named corelist.csv must exist in the same directory.')
         print('The core list should be a csv file with the following format: sectionNumber,coreID')
         print('e.g.: 1,PROJ-LAK17-1A-1P-1-A\n      2,PROJ-LAK17-1A-2B-1-A\n      3,PROJ-LAK17-1A-3B-1-A\n      1,PROJ-LAK17-2A-1P-1-A\n      2,PROJ-LAK17-2A-1P-2-A')
-        exit(-1)
+        exit(1)
 
     # Build the various file names
-    rawFileName = sys.argv[1]
-    matchedFileName = rawFileName[:len(rawFileName)-12] + '.csv' if ('unnamed' in rawFileName) else rawFileName[:len(rawFileName)-4] + '_sectionNames.csv'
-    unmatchedFileName = (rawFileName[:len(rawFileName)-12] if ('unnamed' in rawFileName) else rawFileName[:len(rawFileName)-4]) + '_UNMATCHED.csv'
+    input_filename = sys.argv[1]
+    matched_filename = input_filename[:len(input_filename)-12] + '.csv' if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4] + '_sectionNames.csv'
+    unmatched_filename = (input_filename[:len(input_filename)-12] if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4]) + '_UNMATCHED.csv'
 
-    applyNames(rawFileName, matchedFileName, unmatchedFileName)
+    apply_names(input_filename, matched_filename, unmatched_filename)
