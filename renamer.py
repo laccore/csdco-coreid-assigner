53233bea70e5dc3db4f8169190162fa818ed151d
Alexander Stone <enderu@gmail.com>
2018-01-19 12:03:29 -0600

    Lots of changes: * argparse! Command line usage has descriptions and a huge number of new options * Passing argparse args as kwargs to function as a dict. Feels hacky, but it works. Is there a best practice for this? * More logging if using the -v flag. * Allow control of header and units row, and changing code to handle this. * Specify a file encoding for all read and written files, cleaning up issues around that.

--- renamer.py
+++ renamer.py
@@ -1,8 +1,4 @@
 """
-Both pandas and numpy could do this orders of magnitude more efficiently, but as
-this will be used in the field by people not familiar with python, it's using
-only built-in libraries.
-
 Arguments: input_filename.csv [corelist.csv]
 
 You can specify a core list file name, but if none is specified it looks for a file
@@ -19,41 +15,85 @@ Incorrect command line usage will print an explanation.
 import sys
 import os.path
 import timeit
+import argparse
 
 version = '0.2.0'
 
-start_time = timeit.default_timer()
 
-def apply_names(input_filename, matched_filename, unmatched_filename):
+def apply_names(input_filename, core_list_filename, **kwargs):
+    start_time = timeit.default_timer()
+    if 'kwargs' in kwargs:
+        kwargs = kwargs['kwargs']
+
+    ### Pull out all the options parameters
+    if 'startrow' in kwargs:
+        start_row = kwargs['startrow']
+    else:
+        start_row = 2
+
+    if 'headerrow' in kwargs:
+        header_row = kwargs['headerrow']
+    else:
+        header_row = 0
+
+    if 'unitsrow' in kwargs:
+        units_row = kwargs['unitsrow']
+    else:
+        units_row = 1
+    
+    if kwargs['verbose']:
+        verbose = True
+    else:
+        verbose = False
+    
+    if verbose:
+        print(kwargs)
+
+
+    ### Import the data
     mscl_data = []
     section_list = []
 
-    with open(input_filename, 'r') as f:
-        mscl_data = [r.split(',') for r in f.read().splitlines()]
+    with open(input_filename, 'r', encoding='utf-8-sig') as f:
+        mscl_data = [r.strip().split(',') for r in f.read().splitlines()]
 
-    # Ideally with a GUI these would be fields that could be entered
-    start_row = 2
 
-    # handle failure better
-    if 'SECT NUM' in mscl_data[0]:
-        section_column = mscl_data[0].index('SECT NUM')
-    elif 'Section' in mscl_data[0]:
-        section_column = mscl_data[0].index('Section')
+    if 'sectioncolumn' in kwargs:
+        section_column = kwargs['sectioncolumn']
+        if verbose:
+            print('Section column passed at command line:',section_column)
     else:
-        print('ERROR: Cannot find section number column. Please change section number column name to \'Section\' or \'SECT NUM\'.')
-        exit(1)
+        if 'SECT NUM' in mscl_data[0]:
+            section_column = mscl_data[0].index('SECT NUM')
+            if verbose:
+                print('Section number column found in file with column name \'SECT NUM\':',section_column)
+        elif 'Section' in mscl_data[0]:
+            section_column = mscl_data[0].index('Section')
+            if verbose:
+                print('Section number column found in file with column name \'Section\':',section_column)
+        else:
+            print('ERROR: Cannot find section number column. Please change section number column name to \'Section\' or \'SECT NUM\'.')
+            exit(1)
 
-    # handle failure better
-    if 'Section Depth' in mscl_data[0]:
-        section_depth_column = mscl_data[0].index('Section Depth')
-    elif 'SECT DEPTH' in mscl_data[0]:
-        section_depth_column = mscl_data[0].index('SECT DEPTH')
+    if 'depthcolumn' in kwargs:
+        section_depth_column = kwargs['depthcolumn']
+        if verbose:
+            print('Section depth column passed at command line:',section_depth_column)
     else:
-        print('ERROR: Cannot find section depth column. Please change section depth column name to \'Section Depth\' or \'SECT DEPTH\'.')
-        exit(1)
+        if 'Section Depth' in mscl_data[0]:
+            section_depth_column = mscl_data[0].index('Section Depth')
+            if verbose:
+                print('Section depth column found in file with column name \'Section Depth\':',section_depth_column)
+        elif 'SECT DEPTH' in mscl_data[0]:
+            section_depth_column = mscl_data[0].index('SECT DEPTH')
+            if verbose:
+                print('Section depth column found in file with column name \'SECT DEPTH\':',section_depth_column)
+        else:
+            print('ERROR: Cannot find section depth column. Please change section depth column name to \'Section Depth\' or \'SECT DEPTH\'.')
+            exit(1)
 
     # Build the section list
-    with open(core_list_filename, 'r') as f:
+    with open(core_list_filename, 'r', encoding='utf-8-sig') as f:
         rows = f.read().splitlines()
         section_list = [[int(a), b] for a, b in [r.split(',') for r in rows]]
 
@@ -73,27 +113,32 @@ def apply_names(input_filename, matched_filename, unmatched_filename):
     # Add the part_section notation field to the mscl data
     nSections = 1
     for i, row in enumerate(mscl_data):
-        if i == 0:
+        if i == header_row:
             row.append('Part_Section')
-        elif i < start_row:
+        elif i == units_row:
             row.append('')
         elif i == start_row:
             row.append(str(nSections) + '_' + mscl_data[i][section_column])
-        else:
+        elif i > start_row:
             if int(mscl_data[i][section_column]) < int(mscl_data[i-1][section_column]):
                 nSections += 1
             elif ((int(mscl_data[i][section_column]) == int(mscl_data[i-1][section_column])) & (float(mscl_data[i][section_depth_column]) < float(mscl_data[i-1][section_depth_column]))):
                 nSections += 1
             row.append(str(nSections) + '_' + mscl_data[i][section_column])
+        else:
+            if verbose:
+                print('Ignored row {0} (not header or units row and before start row):\n{1}'.format(i,row))
+
 
     # These will be the lists we export from
     matched_data = []
     unmatched_data = []
 
-    # Copy headers
-    for row in mscl_data[:start_row]:
-        matched_data.append(row[:-1])
-        unmatched_data.append(row)
+    # Add header and unit rows
+    matched_data.append(mscl_data[header_row][:-1])
+    matched_data.append(mscl_data[units_row][:-1])
+    unmatched_data.append(mscl_data[header_row])
+    unmatched_data.append(mscl_data[units_row])
 
     # Build the export lists, replacing the section# with the name and removing
     # the extra part_section column if the row was matched
@@ -106,14 +151,24 @@ def apply_names(input_filename, matched_filename, unmatched_filename):
             unmatched_data.append(row)
     
 
+    if 'outputfilename' in kwargs:
+        matched_filename = kwargs['outputfilename']
+    else:
+        matched_filename = input_filename[:len(input_filename)-12] + '.csv' if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4] + '_sectionNames.csv'
+    
+    if 'unmatchedfilename' in kwargs:
+        unmatched_filename = kwargs['unmatchedfilename']
+    else:
+        unmatched_filename = (input_filename[:len(input_filename)-12] if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4]) + '_UNMATCHED.csv'
+
     # Export matched data
-    with open(matched_filename, 'w') as f:
+    with open(matched_filename, 'w', encoding='utf-8-sig') as f:
         for r in matched_data:
             f.write(','.join(r)+'\n')
 
     # Export unmatched data
     if len(unmatched_data) > start_row:
-        with open(unmatched_filename, 'w') as f:
+        with open(unmatched_filename, 'w', encoding='utf-8-sig') as f:
             for r in unmatched_data:
                 f.write(','.join(r)+'\n')
     
@@ -143,32 +198,40 @@ def apply_names(input_filename, matched_filename, unmatched_filename):
 
     end_time = timeit.default_timer()
 
-    print(len(matched_data)-start_row,'rows had section names assigned (' + matched_filename + ').')
+    print(len(matched_data)-2,'rows had section names assigned (' + matched_filename + ').')
     print('There were no unmatched rows.' if len(unmatched_data) == start_row else 'There were ' + str(len(unmatched_data)-2) + ' unmatched rows (' + unmatched_filename + ').')
-    print('Completed in',round((end_time - start_time),2),'seconds.')    
+    if verbose:
+        print('Completed in',round((end_time - start_time),2),'seconds.')    
 
 
 if __name__ == '__main__':
-    # If zero or more than 2 parameters are passed, print correct usage and exit.
-    if len(sys.argv) > 3 or len(sys.argv) == 1:
-        print('Usage: python renamer.py <MSCL file.csv> (<core list.csv>)')
-        exit(1)
-    
-    # Find the core list
-    # If core list not specified and corelist.csv doesn't exist, tell user it's needed and what it looks like
-    if len(sys.argv) > 2:
-        core_list_filename = sys.argv[2]
+    parser = argparse.ArgumentParser(description='Apply coreIDs to the output from Geotek MSCL software.')
+    parser.add_argument('input_filename', type=str, help='Name of input file.')
+    parser.add_argument('-c', '--corelist', type=str, help='Name of the core list file.')
+    parser.add_argument('-s', '--sectioncolumn', type=int, help='Column number the section numbers are in (count starts at 0).')
+    parser.add_argument('-o', '--outputfilename', type=str, help='Name of the output file.')
+    parser.add_argument('-n', '--unmatchedfilename', type=str, help='Name of the output file for unmatched data.')
+    parser.add_argument('-d', '--depthcolumn', type=int, help='Column number the section depths are in (count starts at 0).')
+    parser.add_argument('-r', '--startrow', type=int, help='Row number the MSCL data begins (count starts at 0).')
+    parser.add_argument('-t', '--headerrow', type=int, help='Row number the headers are on (count starts at 0).')
+    parser.add_argument('-u', '--unitsrow', type=int, help='Row number the units are on (count starts at 0).')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increase verbosity.')
+
+    args = parser.parse_args()
+
+    if args.corelist:
+        core_list_filename = args.corelist
     elif os.path.isfile('corelist.csv'):
         core_list_filename = 'corelist.csv'
     else:
-        print('A core list file needs to be specified as the last argument, or a file named corelist.csv must exist in the same directory.')
+        print('A core list file needs to be specified (e.g. python renamer.py -c PRJ_core_list.csv).')
+        print('Alternatively, a file named corelist.csv must exist in the same directory.')
         print('The core list should be a csv file with the following format: sectionNumber,coreID')
         print('e.g.: 1,PROJ-LAK17-1A-1P-1-A\n      2,PROJ-LAK17-1A-2B-1-A\n      3,PROJ-LAK17-1A-3B-1-A\n      1,PROJ-LAK17-2A-1P-1-A\n      2,PROJ-LAK17-2A-1P-2-A')
         exit(1)
 
-    # Build the various file names
-    input_filename = sys.argv[1]
-    matched_filename = input_filename[:len(input_filename)-12] + '.csv' if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4] + '_sectionNames.csv'
-    unmatched_filename = (input_filename[:len(input_filename)-12] if ('unnamed' in input_filename) else input_filename[:len(input_filename)-4]) + '_UNMATCHED.csv'
+    # Build dict of specified optional parameters to pass to function
+    # Is there a best practice to handle this? seems pretty hacky
+    argsDict = {k:v for k,v in vars(args).items() if v is not None}
 
-    apply_names(input_filename, matched_filename, unmatched_filename)
+    apply_names(args.input_filename, core_list_filename, kwargs=argsDict)
